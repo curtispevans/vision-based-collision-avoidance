df70e07b261335fc89806fdaf5c686af143b2f75
curtis <curtispevans@gmail.com>
2025-07-14 09:56:11 -0600

    helping szu wei

--- deliverable_code/params.py
+++ deliverable_code/params.py
@@ -1,9 +1,9 @@
 
 # global variables
 ts_simulation = 1/30
-min_area = 10
-max_area = 25
+min_area = 9
+max_area = 11
 bearing_uncertainty = 0.1
 measured_window = 30 # number of frames
 dim_astar = 25
-solution_span = 1000
+solution_span = 500
